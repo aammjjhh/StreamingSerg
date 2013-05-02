0d9f4a24373275322ee5f5763975aaa0332a7623
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:47:56 -0500

    Update SS_watercolor.php

--- SS_watercolor.php
+++ SS_watercolor.php
@@ -1,10 +1,10 @@
 <?php
 require_once("my_view/gheaderView.php");
-  echo '				<div id="galleria">' . PHP_EOL;
+ 	echo '				<div id="galleria">' . PHP_EOL;
 	echo '					<img src="my_images/Nitrogen_Cycle.jpg">' . PHP_EOL;
 	echo '					<img src="my_images/Assassin_Mask.jpg">' . PHP_EOL;
 	echo '					<img src="my_images/Eternal_Bliss.jpg">' . PHP_EOL;
 	echo '					<img src="my_images/Father_of_Foundation.jpg">' . PHP_EOL;
-    echo '				</div>' . PHP_EOL;
+   	echo '				</div>' . PHP_EOL;
 require_once("my_view/gfooterView.php");
 ?>
