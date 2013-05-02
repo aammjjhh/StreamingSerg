d21168b2eb4070956e317b8c093773b3a6081248
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:40:42 -0500

    Update SS.php

--- SS.php
+++ SS.php
@@ -1,54 +1,53 @@
 <?php
 require_once("my_view/mheaderView.php");
 ?>
-  		</div>
-			<!-- begin hax -->
-				<div id="homepage-box-1" class="grid_4 hackbox">
-					<img class="first" src="my_images/Father_of_Foundation.jpg" height="300" width="300" border='0' alt="Father of Foundation"/>
-				</div>
-				<div id="homepage-box-2" class="grid_4 hackbox">
-					<img  src="my_images/Baby_Khayla.jpg" height="300" width="300" border='0' alt="Baby Khayla"/>
-				</div>
-				<div id="homepage-box-3" class="grid_4 hackbox">
-					<img  src="my_images/Collage.jpg" height="300" width="300" border='0' alt="Collage"/>
-				</div>
-				<div class="grid_12" id="humenews-box">
-					<h3>
-						<span>Art Gallery</span>
-					</h3>
-					<div id="humenews-news" class="humenews-block">
-						<h4>Oil on Canvas</h4>
-						<ul>
-							<li><a href='SS_oil.php'>A Man is Not Measured...</a></li>
-							<li><a href='SS_oil.php'>Bucket and Mannequin</a></li>
-							<li><a href='SS_oil.php'>Collage</a></li>
-						</ul>
-					</div>
-					<div id="humenews-photos" class="humenews-block middle">
-						<h4>Pencil Drawings</h4>
-						<ul>
-							<li><a href='SS_pencil.php'>Falcon Punch</a></li>
-							<li><a href='SS_pencil.php'>Mystical Staff</a></li>
-							<li><a href='SS_pencil.php'>Baby Khayla</a></li>
-							<li><a href='SS_pencil.php'>Pretty Lady</a></li>
-							<li><a href='SS_pencil.php'>Happy Dog</a></li>
-							<li><a href='SS_pencil.php'>Arm Canon</a></li>
-							<li><a href='SS_pencil.php'>Tri-shape</a></li>
-						</ul>
-					</div>
-
-					<div id="humenews-wrapups" class="humenews-block">
-						<h4>Watercolor and Colored Pencil</h4>
-						<ul>
-							<li><a href='SS_watercolor.php'>The Nitrogen Cycle</a></li>
-							<li><a href='SS_watercolor.php'>Assassin Mask</a></li>
-							<li><a href='SS_watercolor.php'>Eternal Bliss</a></li>
-							<li><a href='SS_watercolor.php'>Father of Foundation</a></li>
-						</ul>
-					</div>
-				</div>
-				<div>
-			<!-- end hax -->
+</div>
+<!-- begin hax -->
+	<div id="homepage-box-1" class="grid_4 hackbox">
+		<img class="first" src="my_images/Father_of_Foundation.jpg" height="300" width="300" border='0' alt="Father of Foundation"/>
+	</div>
+	<div id="homepage-box-2" class="grid_4 hackbox">
+		<img  src="my_images/Baby_Khayla.jpg" height="300" width="300" border='0' alt="Baby Khayla"/>
+	</div>
+	<div id="homepage-box-3" class="grid_4 hackbox">
+		<img  src="my_images/Collage.jpg" height="300" width="300" border='0' alt="Collage"/>
+	</div>
+	<div class="grid_12" id="humenews-box">
+		<h3>
+			<span>Art Gallery</span>
+		</h3>
+		<div id="humenews-news" class="humenews-block">
+			<h4>Oil on Canvas</h4>
+			<ul>
+				<li><a href='SS_oil.php'>A Man is Not Measured...</a></li>
+				<li><a href='SS_oil.php'>Bucket and Mannequin</a></li>
+				<li><a href='SS_oil.php'>Collage</a></li>
+			</ul>
+		</div>
+		<div id="humenews-photos" class="humenews-block middle">
+			<h4>Pencil Drawings</h4>
+			<ul>
+				<li><a href='SS_pencil.php'>Falcon Punch</a></li>
+				<li><a href='SS_pencil.php'>Mystical Staff</a></li>
+				<li><a href='SS_pencil.php'>Baby Khayla</a></li>
+				<li><a href='SS_pencil.php'>Pretty Lady</a></li>
+				<li><a href='SS_pencil.php'>Happy Dog</a></li>
+				<li><a href='SS_pencil.php'>Arm Canon</a></li>
+				<li><a href='SS_pencil.php'>Tri-shape</a></li>
+			</ul>
+		</div>
+		<div id="humenews-wrapups" class="humenews-block">
+			<h4>Watercolor and Colored Pencil</h4>
+			<ul>
+				<li><a href='SS_watercolor.php'>The Nitrogen Cycle</a></li>
+				<li><a href='SS_watercolor.php'>Assassin Mask</a></li>
+				<li><a href='SS_watercolor.php'>Eternal Bliss</a></li>
+				<li><a href='SS_watercolor.php'>Father of Foundation</a></li>
+			</ul>
+		</div>
+	</div>
+	<div>
+	<!-- end hax -->
 <?php
 require_once("my_view/mfooterView.php");
 ?>
