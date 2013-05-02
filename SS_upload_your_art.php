3a6eeee79bbcd85c9c4583f645d7126ef87fa56c
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:51:43 -0500

    Create SS_upload_your_art.php

---
+++ SS_upload_your_art.php
@@ -0,0 +1,7 @@
+<?php
+require_once("my_view/mheaderView.php");
+  echo '				<h2>Upload Your Art</h2>' . PHP_EOL;
+	echo '			    <h4>Post your own pictures!</h4>' . PHP_EOL;
+	require_once("SS_users.php");
+require_once("my_view/mfooterView.php");
+?>	
