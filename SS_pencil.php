1f710bda7e5c9d977cd427419ccd1dec2d504bb8
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:49:15 -0500

    Update SS_pencil.php

--- SS_pencil.php
+++ SS_pencil.php
@@ -1,13 +1,13 @@
 <?php
 require_once("my_view/gheaderView.php");
-    echo '				<div id="galleria">' . PHP_EOL;
-	  echo '					<img src="my_images/Falcon_Punch.jpg">' . PHP_EOL;
-	  echo '					<img src="my_images/Mystical_Staff.jpg">' . PHP_EOL;
-	  echo '					<img src="my_images/Baby_Khayla.jpg">' . PHP_EOL;
-	  echo '					<img src="my_images/Pretty_Lady.jpg">' . PHP_EOL;
-	  echo '					<img src="my_images/Happy_Dog.jpg">' . PHP_EOL;
-	  echo '					<img src="my_images/Arm_Canon.jpg">' . PHP_EOL;
-	  echo '					<img src="my_images/Tri-shape.jpg">' . PHP_EOL;
+    echo '				<div id="galleria">' . PHP_EOL; 
+    echo '					<img src="my_images/Falcon_Punch.jpg">' . PHP_EOL;
+    echo '					<img src="my_images/Mystical_Staff.jpg">' . PHP_EOL;
+    echo '					<img src="my_images/Baby_Khayla.jpg">' . PHP_EOL;
+    echo '					<img src="my_images/Pretty_Lady.jpg">' . PHP_EOL;
+    echo '					<img src="my_images/Happy_Dog.jpg">' . PHP_EOL;
+    echo '					<img src="my_images/Arm_Canon.jpg">' . PHP_EOL;
+    echo '					<img src="my_images/Tri-shape.jpg">' . PHP_EOL;
     echo '				</div>' . PHP_EOL;
 require_once("my_view/gfooterView.php");
 ?>
