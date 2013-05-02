3220887daaa85f1ac11e1234b41073a9e4d52287
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:44:11 -0500

    Create SS_about_sergio.php

---
+++ SS_about_sergio.php
@@ -0,0 +1,9 @@
+<?php
+require_once("my_view/mheaderView.php");
+  echo '				<h2>About Sergio</h2>' . PHP_EOL;
+	echo '				<!-- tab containers -->' . PHP_EOL;
+	echo '				<div class="pane">' . PHP_EOL;
+	echo '					<p>Sergio Quintanilla was born in Houston, TX on October 19, 1989. He lived with his parents in Mexico for his first 4 years and moved to Houston with his mother following his parents’ separation. He was raised by his single parent mother and is the oldest of three siblings. His passion for creating artwork comes from the fundamentals of drawing taught to him by his mother before he learned to read or write. He spent most of his youth indoors playing video games and watching cartoons, from where much of the creativity in his work comes. He has learned very important lessons about having an artistic instinct from several great artists that have taken him under their wing. However, through his journey in the path of mastery of the visual arts, the most important lessons he knows to be true about creating art have been self-taught. Sergio is currently a senior attending Vanderbilt University with a major in Mechanical Engineering and a minor in Art. He aims to continue his studies and graduate from a great school of design, where he hopes to learn more about creating art and designing ideas that will help the people in his lifetime and beyond.</p>' . PHP_EOL;
+	echo '				</div>' . PHP_EOL;
+require_once("my_view/mfooterView.php");
+?>
