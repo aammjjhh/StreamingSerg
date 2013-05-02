14e0791be56980940f15ae9efc5a4d5b4641febf
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:44:59 -0500

    Create SS_contact.php

---
+++ SS_contact.php
@@ -0,0 +1,11 @@
+<?php
+require_once("my_view/mheaderView.php");
+  echo '				<h2>Contact Sergio</h2>' . PHP_EOL;
+	echo '				<!-- tab containers -->' . PHP_EOL;
+	echo '				<div class="pane">' . PHP_EOL;
+	echo '					<h3>Contact</h3>' . PHP_EOL;
+	echo '					<h4>Phone: 281.772.9421</h4>' . PHP_EOL;
+	echo '					<p>You can contact Sergio via a phone call, text message, or message on Facebook.</p>' . PHP_EOL;
+	echo '				</div>' . PHP_EOL;
+require_once("my_view/mfooterView.php");
+?>
