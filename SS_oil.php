b202a95eb8291198c790dd4cedd1910dc9a7659f
Amanda Hatfield <amanda.j.hatfield@vanderbilt.edu>
2013-05-02 02:46:34 -0500

    Create SS_oil.php

---
+++ SS_oil.php
@@ -0,0 +1,9 @@
+<?php
+require_once("my_view/gheaderView.php");
+  echo '				<div id="galleria">' . PHP_EOL;
+	echo '					<img src="my_images/Man_Head.jpg" data-description="Man is not measured...">' . PHP_EOL;
+	echo '					<img src="my_images/Bucket_and_Mannequin.jpg" data-description="Bucket and Mannequin">' . PHP_EOL;
+	echo '					<img src="my_images/Collage.jpg" data-description="Collage">' . PHP_EOL;
+    echo '				</div>' . PHP_EOL;
+require_once("my_view/gfooterView.php");
+?>
